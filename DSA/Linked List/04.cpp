a07d57e1842507a56d844b8fda7a1c42bc5dfb3c
ritik <kumarritikworldno1@gmail.com>
2026-02-05 23:22:52 +0530

    added new code

--- DSA/Linked List/04.cpp
+++ DSA/Linked List/04.cpp
@@ -17,6 +17,38 @@ public:
     return SearchKey(head->next, key);
    }
 
+   class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        //Dummy nodes
+        ListNode* left = new ListNode(-1);
+        ListNode* right = new ListNode(-1);
+
+        ListNode* lp = left;
+        ListNode* rp = right;
+
+        ListNode* p1 = head;
+
+
+        while(p1 != NULL){
+            if(p1->val < x){
+                left->next = p1;
+                left = left->next;
+            }else{
+                right->next = p1;
+                right = right->next;
+            }
+            p1 = p1->next;
+
+        }
+        right->next = NULL;
+
+        left->next = rp->next;
+
+        return lp->next;
+    }
+};
+
 };
 int main(){
    
