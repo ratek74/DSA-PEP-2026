cccd7ce25c4cba9b0d536cec8da45d37681c5d9c
ritik <kumarritikworldno1@gmail.com>
2026-02-12 22:33:13 +0530

    new files addes

---
+++ DSA/Trees/01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+Tree -> It is a nom - linear , hierarchical data structure consisting of node connnected by edges
+Binary tree -> a tree is a binary tree when every node has at most two children.
+
+Termonologies;-
+
+1. leaf - a node with no children
+2. internal node - a node which is not a leaf
+3. siblings - the children of same parent
+4. Length path - length of path is  1 less then the number of nodes on the path
+5. Ancestors and Decendent - if there is a path from node A to node B , then A is called an ancestors of B and B is called
+descendent of A.
+6. Subtree - any node of a tree with all of its descendents is a subtree
+7. level - the level of the  node referes to its distance from the root, the root of the tree has generally
+level 0;
+*/
