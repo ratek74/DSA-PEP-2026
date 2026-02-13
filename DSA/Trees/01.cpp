6449dbfd39d5b1a87fdcea4ff1c7e06141315ea5
ritik <kumarritikworldno1@gmail.com>
2026-02-13 22:14:10 +0530

    new files added

--- DSA/Trees/01.cpp
+++ DSA/Trees/01.cpp
@@ -11,7 +11,8 @@ Termonologies;-
 2. internal node - a node which is not a leaf
 3. siblings - the children of same parent
 4. Length path - length of path is  1 less then the number of nodes on the path
-5. Ancestors and Decendent - if there is a path from node A to node B , then A is called an ancestors of B and B is called
+5. Ancestors and Decendent - if there is a path from node 
+A to node B , then A is called an ancestors of B and B is called
 descendent of A.
 6. Subtree - any node of a tree with all of its descendents is a subtree
 7. level - the level of the  node referes to its distance from the root, the root of the tree has generally
