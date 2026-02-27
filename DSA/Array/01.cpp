7df2782848ddb3d218011a5ccf245d174fa839e2
ritik <kumarritikworldno1@gmail.com>
2026-02-27 21:57:57 +0530

    new code added

--- DSA/Array/01.cpp
+++ DSA/Array/01.cpp
@@ -10,3 +10,33 @@
 //             j++;
 //         }
 //        }
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int age = 5.5;
+    // float num1 = 5.5;
+    // cout << num1;
+
+    int a = 5;
+    int b = 10;
+
+    //operators
+    //Arithematic -> + - / * %
+    // cout << a + b << endl;
+    // a++;
+    // cout << a++ + ++b << endl;
+
+    //Relational -> >, >=, <, <=, ==(comparison)
+    // cout << (10 == 5) << endl; 
+
+    //Logical
+
+    int age = 25;
+    bool hasID = true;
+
+    if(age < 1 && hasID){
+        
+    }
+
+}
